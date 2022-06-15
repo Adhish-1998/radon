350f3e3c6edea441fe8134d9da198637cdafb94a
Adhish Basak <adhishbasak98@gmail.com>
2022-06-15 21:20:25 +0530

    Final Push

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,26 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+const jwt = require('jsonwebtoken')
 
+
+const authenticate = function(req, res, next) {
+    let token = req.headers["x-Auth-token"];
+    if(!token) token = req.headers["x-auth-token"];
+    if(!token) return res.send({ status: false, msg: "token must be present" }); 
+    let decodedToken = jwt.verify(token, "functionup-radon");
+    if(!decodedToken) return res.send({ status: false, msg: "token is invalid" });
     next()
 }
 
 
 const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
+    let token = req.headers["x-Auth-token"];
+    if(!token) token = req.headers["x-auth-token"];
+    let decodedToken = jwt.verify(token, "functionup-radon");
+    let userToBeModified = req.params.userId
+    let userLoggedIn = decodedToken.userId
+    if(userToBeModified != userLoggedIn) return res.send({status : false, msg: 'You are not Authorised'})
     next()
 }
+
+
+module.exports.authenticate = authenticate
+module.exports.authorise = authorise
