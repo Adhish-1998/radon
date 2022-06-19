67d34e06f1ec4cedbe846457dd7cdd4f247381d2
Adhish Basak <adhishbasak98@gmail.com>
2022-06-19 22:57:40 +0530

    Final Push

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -5,7 +5,7 @@ const productModel = require("../models/productModel")
 
 
 const createOrder= async function (req, res) {
-    
+        
     if(!req.body.userId) res.send({msg: "UserId Requrired !!!"})
     if(!req.body.productId) res.send({msg: "ProductId Required !!!"})
 
@@ -13,23 +13,24 @@ const createOrder= async function (req, res) {
     if(!userId) res.send({msg: "User Does Not Exist !!!"})
     let productId= await productModel.findById(req.body.productId)
     if(!productId) res.send({msg : "Product Does Not Exist !!!"})
-    
+    let freeapp = await userModel.findOne(userId).select({isFreeAppUser : 1, _id:0})
     let userBalance = await userModel.findOne(userId).select({balance:1, _id:0})
     let productPrice = await productModel.findOne(productId).select({price:1, _id:0})
+    
     //First Scenario is done
-    if(userBalance.balance >= productPrice.price){
+    if((userBalance.balance < productPrice.price) && (freeapp.isFreeAppUser == false)){
+        res.send({msg : 'Insufficient Balance.'})
+    }else if((userBalance.balance >= productPrice.price)&&((freeapp.isFreeAppUser == false))){
         userBalance.balance = userBalance.balance - productPrice.price
         await userModel.findOneAndUpdate({_id : userId}, {$set: {balance: userBalance.balance} })
         req.body.amount = productPrice.price
         let savedOrder = await orderModel.create(req.body)
-        savedOrder = await savedOrder.populate('userId')
+        savedOrder = await savedOrder
         res.send({msg : savedOrder})
-    }
-    //res.send({msg : "Arrived in End Line !!!"})
-    // let order = req.body
-    // let savedOrder = await orderModel.create(order)
-    // res.send({msg: savedOrder})
-
+        }else{
+            let savedOrder = await orderModel.create(req.body)       
+            res.send({msg : savedOrder})
+        }
 }
 
 
