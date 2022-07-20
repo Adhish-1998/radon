c4e888f67e80ee2c958705ab17b81597cc61f9a8
Adhish Basak <adhishbasak98@gmail.com>
2022-07-20 17:34:09 +0530

    AWS Session

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-
 const route = require('./routes/route.js');
-
 const app = express();
-
 const multer= require("multer");
 const { AppConfig } = require('aws-sdk');
 
